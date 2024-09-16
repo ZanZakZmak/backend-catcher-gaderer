892e2dc7538d57d86a81a4da535b23143690e810
ZanZakZmak <58043160+ZanZakZmak@users.noreply.github.com>
2024-09-17 00:24:51 +0200

    added time filter and delete post

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config(); // učitava environment varijable iz datoteke .env
 import express from "express";
-
 import cors from "cors";
 import connect from "./DB";
 //import * as dataHandlers from "./handlers/dataHandlers.js";
@@ -41,61 +40,14 @@ app.post("/auth", async (req, res) => {
     res.status(401).json({ error: error.message });
   }
 });
-//example of middleweare
-
-//social posts
-//sa js filteranje
-/*app.get("/posts", async (req, res) => {
-  let query = req.query;
-  console.log("da vidimo querry:", query);
-  //with mongo querry object
-  let filter;
-  try {
-    let db = await connect(); // pristup db objektu
-    let cursor = await db.collection("posts").find();
-    let results = await cursor.toArray();
-    console.log("testic", results);
-    //filter with javascript
-    if (query.search && !(query.search === "") && query.search != "null") {
-      results = results.filter((element) => {
-        return !(
-          element.title.toLowerCase().search(query.search.toLowerCase()) === -1
-        );
-      });
-    }
-    if (
-      query.categoryFilter &&
-      query.categoryFilter != "" &&
-      query.categoryFilter != "null"
-    ) {
-      let category = query.categoryFilter.split(",");
-      console.log("ovo je moj querry :", category);
-      function checkCategory(dbCat, searchCat) {
-        let result = false;
-        for (let i = 0; i < searchCat.length; i++) {
-          if (dbCat.includes(searchCat[i].toLowerCase())) {
-            result = true;
-          }
-        }
-        return result;
-      }
-      results = results.filter((element) => {
-        return checkCategory(element.category, category);
-      });
-    }
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ errors: error });
-  }
-});*/
-app.get("/posts/:type", async (req, res) => {
+//social posts i info posts
+app.get("/posts/:type", [auth.verify], async (req, res) => {
   // nepostojeći querry je samo undefined
   let query = req.query;
   let pharams = req.params;
-  console.log("exsperiment", pharams.type);
-  console.log("state iner:", query);
+
   try {
-    let filtersExample = {
+    /*let filtersExample = {
       //and on wat things
       $and: [
         { type: new RegExp("social") },
@@ -106,22 +58,11 @@ app.get("/posts/:type", async (req, res) => {
         //area filter
         { area: new RegExp("pula") },
       ],
-    };
+    };*/
     let filters = {
       $and: [{ type: new RegExp(pharams.type) }],
     };
 
-    /* if (
-      (query.search && !(query.search === "")) ||
-      (query.categoryFilter &&
-        query.categoryFilter != "" &&
-        query.categoryFilter != "null") ||
-      (query.areaFilter &&
-        !(query.areaFilter === "") &&
-        query.areaFilter != "null")
-    ) {
-      filters.$and = [];
-    }*/
     //search term
     if (query.search && !(query.search === "")) {
       filters.$and.push({
@@ -152,6 +93,26 @@ app.get("/posts/:type", async (req, res) => {
     ) {
       filters.$and.push({ area: new RegExp(query.areaFilter, "i") });
     }
+    // time span filter span
+    if (
+      query.startTimeFilter &&
+      query.endTimeFilter &&
+      !(query.startTimeFilter === "") &&
+      !(query.endTimeFilter === "") &&
+      query.startTimeFilter != "null" &&
+      query.endTimeFilter != "null"
+    ) {
+      console.log("desio sam se", typeof query.startTimeFilter);
+      //startTimeFilter
+      //endTimeFilter
+
+      filters.$and.push({
+        createdTime: {
+          $gt: Number(query.startTimeFilter),
+          $lte: Number(query.endTimeFilter),
+        },
+      });
+    }
     //info type for info
     //
     if (
@@ -174,7 +135,7 @@ app.get("/posts/:type", async (req, res) => {
   }
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts", [auth.verify], async (req, res) => {
   var postData = req.body;
   let db = await connect(); // pristup db objektu
   let doc = {
@@ -208,9 +169,9 @@ app.post("/posts", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-//single socialpost
+//single post
 //pazi vec pstoji get.posts sa pharams /: tako da ova ruta mora biti drugacija
-app.get("/post/:id", async (req, res) => {
+app.get("/post/:id", [auth.verify], async (req, res) => {
   let id = req.params.id;
 
   try {
@@ -223,7 +184,7 @@ app.get("/post/:id", async (req, res) => {
     let cursor = await db
       .collection("comments")
       .find({ postId: id }) //new ObjectId(id)
-      .sort({ createdTime: 1 }); //.sort( { postedAt: 1 })
+      .sort({ createdTime: -1 }); //.sort 1 or -1
     let commentArr = await cursor.toArray();
     //prebaciti cu u id samo da ne bude _
     commentArr.forEach((element) => {
@@ -239,26 +200,41 @@ app.get("/post/:id", async (req, res) => {
     res.status(500).json({ errors: error });
   }
 });
-app.delete("/posts/:id", (req, res) => {
-  res.status(200).json({});
-});
-//comments
-/*
-app.get("/post/:postid/comments", async (req, res) => {
-  let id = req.params.postid;
+app.delete("/post/:id", [auth.verify], async (req, res) => {
+  let postid = req.params.id;
+  let userid = req.jwt._id;
+
   try {
     let db = await connect(); // pristup db objektu
-    let cursor = await db
-      .collection("comments")
-      .find({ postId: id }) //new ObjectId(id)
-      .sort({ createdTime: 1 }); //.sort( { postedAt: 1 })
-    let results = await cursor.toArray();
-    res.status(200).json(results);
+    //remove from post
+    let cursor = await db.collection("posts");
+    let postInfo = await cursor.findOne({ _id: new ObjectId(postid) });
+    console.log("post info", postInfo);
+
+    if (postInfo.createdById == userid) {
+      console.log("ja sam created", postInfo.createdById);
+      console.log("ja sam poslan", userid);
+      //remove comments
+      let cursorComents = await db.collection("comments");
+      postInfo.comments.forEach(async (element) => {
+        console.log("ele u coments", element);
+
+        cursorComents.deleteOne({ _id: new ObjectId(element) });
+      });
+      //remove post
+      await cursor.deleteOne({ _id: new ObjectId(postid) });
+    } else {
+      res.status(500).json({ error: "nije vas post" });
+    }
+
+    res.sendStatus(200);
   } catch (error) {
-    res.status(500).json({ errors: error });
+    res.status(500).json({ error: error });
   }
-});*/
-app.post("/posts/:postid/comments", async (req, res) => {
+});
+//comments
+
+app.post("/posts/:postid/comments", [auth.verify], async (req, res) => {
   let id = req.params.postid;
   var postData = req.body;
   let doc = {
@@ -289,48 +265,38 @@ app.post("/posts/:postid/comments", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-app.delete("/posts/:postid/comments/:comid", async (req, res) => {
-  let postId = req.params.postid;
-  let commentId = req.params.comid;
+app.delete(
+  "/posts/:postid/comments/:comid",
+  [auth.verify],
+  async (req, res) => {
+    let postId = req.params.postid;
+    let commentId = req.params.comid;
 
-  try {
-    let db = await connect(); // pristup db objektu
-    //remove from post
-    let resultUpdate = await db.collection("posts").updateOne(
-      { _id: new ObjectId(postId) },
+    try {
+      let db = await connect(); // pristup db objektu
+      //remove from post
+      let resultUpdate = await db.collection("posts").updateOne(
+        { _id: new ObjectId(postId) },
 
-      { $pull: { comments: new ObjectId(commentId) } }
-    );
-    //remove comment
-    await db.collection("comments").deleteOne({ _id: new ObjectId(commentId) });
+        { $pull: { comments: new ObjectId(commentId) } }
+      );
+      //remove comment
+      await db
+        .collection("comments")
+        .deleteOne({ _id: new ObjectId(commentId) });
 
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(500).json({ errors: error });
+      res.sendStatus(200);
+    } catch (error) {
+      res.status(500).json({ errors: error });
+    }
   }
-});
-//info posts
+);
+
 //encyclopedia
-app.get("/encyclopedia", async (req, res) => {
+app.get("/encyclopedia", [auth.verify], async (req, res) => {
   let query = req.query;
   console.log("state iner:", query);
   try {
-    let filtersExample = {
-      //and on wat things
-      $and: [
-        {
-          $or: [
-            { name: new RegExp("ulov", "i") },
-            { nameLat: new RegExp("ulov", "i") },
-            { namesAlt: new RegExp("ulov", "i") },
-          ],
-        },
-        // category filter
-        { category: { $all: ["herb", "fungi"] } },
-        //area filter
-        { poisonous: new RegExp("pula", "i") },
-      ],
-    };
     let filters = {};
 
     if (
@@ -381,7 +347,7 @@ app.get("/encyclopedia", async (req, res) => {
 
     let db = await connect(); // pristup db objektu
     let cursor = await db.collection("encyclopedia").find(filters);
-    //.sort({ createdTime: 1 }); //.sort( { postedAt: 1 })
+
     let results = await cursor.toArray();
     res.status(200).json(results);
   } catch (error) {
@@ -389,7 +355,7 @@ app.get("/encyclopedia", async (req, res) => {
   }
 });
 
-app.post("/encyclopedia", async (req, res) => {
+app.post("/encyclopedia", [auth.verify], async (req, res) => {
   var postData = req.body;
 
   let doc = {
@@ -415,7 +381,7 @@ app.post("/encyclopedia", async (req, res) => {
   }
 });
 //single encyclopedia
-app.get("/encyclopedia/:id", async (req, res) => {
+app.get("/encyclopedia/:id", [auth.verify], async (req, res) => {
   let id = req.params.id;
 
   try {
@@ -433,9 +399,8 @@ app.get("/encyclopedia/:id", async (req, res) => {
 //profile
 app.patch("/user/password", [auth.verify], async (req, res) => {
   let changes = req.body;
-  //PROBLEM
+
   let id = req.jwt._id;
-  console.log("stali je tu u tokenu", req.jwt);
 
   if (changes.newPassword && changes.oldPassword) {
     let result = await auth.changeUserPassword(
@@ -455,9 +420,8 @@ app.patch("/user/password", [auth.verify], async (req, res) => {
 });
 app.patch("/user/username", [auth.verify], async (req, res) => {
   let changes = req.body;
-  //PROBLEM
+
   let id = req.jwt._id;
-  console.log("stali je tu u tokenu", req.jwt);
 
   if (changes.newUsername && changes.oldPassword) {
     let result = await auth.changeUserUsername(
@@ -469,7 +433,6 @@ app.patch("/user/username", [auth.verify], async (req, res) => {
     if (result) {
       res.status(201).send();
     } else {
-      console.log("ja sammm");
       res.status(500).json({ error: "canot change username" });
     }
   } else {
@@ -478,7 +441,6 @@ app.patch("/user/username", [auth.verify], async (req, res) => {
 });
 app.patch("/user/email", [auth.verify], async (req, res) => {
   let changes = req.body;
-  //PROBLEM
   let id = req.jwt._id;
   console.log("stali je tu u tokenu", req.jwt);
 
@@ -498,127 +460,4 @@ app.patch("/user/email", [auth.verify], async (req, res) => {
   }
 });
 
-//testovi
-app.get("/testQuery", async (req, res) => {
-  let query = req.query;
-  console.log("state iner:", query);
-  try {
-    let filtersExample = {
-      //and on wat things
-      $and: [
-        { title: new RegExp("ulov") },
-        // category filter
-        { category: { $all: ["herb", "fungi"] } },
-        //area filter
-        { area: new RegExp("pula") },
-      ],
-    };
-    let filters = {};
-
-    if (
-      (query.search && !(query.search === "")) ||
-      (query.categoryFilter &&
-        query.categoryFilter != "" &&
-        query.categoryFilter != "null") ||
-      (query.areaFilter &&
-        !(query.areaFilter === "") &&
-        query.areaFilter != "null")
-    ) {
-      filters.$and = [];
-    }
-    //search term
-    if (query.search && !(query.search === "")) {
-      filters.$and.push({
-        $or: [
-          { title: new RegExp(query.search, "i") },
-          { createdBy: new RegExp(query.search, "i") },
-        ],
-      });
-    }
-    //category filter
-    if (
-      query.categoryFilter &&
-      query.categoryFilter != "" &&
-      query.categoryFilter != "null"
-    ) {
-      let category = query.categoryFilter.split(",");
-
-      filters.$and.push({ category: { $all: [...category] } });
-    }
-
-    //area filter
-    if (
-      query.areaFilter &&
-      !(query.areaFilter === "") &&
-      query.areaFilter != "null"
-    ) {
-      filters.$and.push({ area: new RegExp(query.areaFilter, "i") });
-    }
-
-    let db = await connect(); // pristup db objektu
-    let cursor = await db.collection("posts").find(filters);
-    let results = await cursor.toArray();
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ errors: error });
-  }
-});
-app.get("/tesiranjeMongoTAN", [auth.verify], async (req, res) => {
-  //let results = { kako: "ono" };
-  try {
-    let db = await connect(); // pristup db objektu
-    let cursor = await db.collection("test1").find();
-    let results = await cursor.toArray();
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ errors: error });
-  }
-});
-app.post("/tesiranjeMongoTAN", [auth.verify], async (req, res) => {
-  let body = req.body;
-  if (Object.keys(body).length != 0) {
-    let db = await connect(); // pristup db objektu
-    let Collection = await db.collection("test1");
-    Collection.insertOne(body);
-    res.status(200).json(body);
-  } else {
-    res.sendStatus(400);
-  }
-});
-app.patch("/tesiranjeMongoTAN", [auth.verify], async (req, res) => {
-  //id=req.params.id
-  let db = await connect(); // pristup db objektu
-  let Collection = await db.collection("test1");
-
-  await Collection.updateOne(
-    { _id: new ObjectId("65a133559c26c0de372e4224") },
-
-    { $set: { name: "Kumerle2" } }
-  );
-  res.sendStatus(200);
-});
-app.delete("/tesiranjeMongoTAN/:id", [auth.verify], async (req, res) => {
-  let id = req.params.id;
-  try {
-    let db = await connect(); // pristup db objektu
-    await db.collection("test1").deleteOne({ _id: new ObjectId(id) });
-
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(500).json({ errors: error });
-  }
-});
-
-/*async function _getTest() {
-  const result = await Collection.find({}).toArray();
-  //id: { $in: movieData }
-  return result;
-}
-app.get("/testiranjeMongo", async (req, res) => {
-  //const Db = await connect.db;
-  //const collection = db.collection("test1");
-  let result = await _getTest();
-  console.log("db Test:", result);
-  res.status(200).json(result);
-});*/
 app.listen(port, () => console.log(`Slušam na portu ${port}!`));
